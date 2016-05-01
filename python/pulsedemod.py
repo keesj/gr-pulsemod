27fbf725551e08ffc12e6ffa5fefe194893fb58d
Kees Jongenburger <kees.jongenburger@gmail.com>
2016-05-01 22:02:16 +0200

    Minor changes

--- python/pulsedemod.py
+++ python/pulsedemod.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2015 kees Jongenburger
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 import numpy
 from gnuradio import gr
@@ -65,13 +65,13 @@ class pulsedemod(gr.sync_block):
             if sample < 0.5 and self.upcount > 0:
                 self.end_sample(1,self.upcount)
                 self.upcount = 0
-            if sample > 0.5:
+            if sample >= 0.5:
                 self.upcount += 1
 
             # do downcount
             if sample > 0.5 and self.downcount > 0:
                 self.end_sample(0,self.downcount)
                 self.downcount = 0
-            if sample < 0.5:
+            if sample <= 0.5:
                 self.downcount += 1
         return len(input_items[0])
